68dd17484df7631376232ff00d1847a9f7c1df8d
Mahaut Windal <mwindal@hotmail.com>
2026-03-20 16:08:23 +0100

    Comment out CSRF_TRUSTED_ORIGINS setting for local development

--- job_board/settings.py
+++ job_board/settings.py
@@ -27,7 +27,7 @@ DEBUG = os.getenv('APP_DEBUG', 'False') == True
 
 ALLOWED_HOSTS = ['127.0.0.1', os.getenv('APP_DOMAIN','localhost')]
 
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'https://'+ os.getenv('APP_DOMAIN','localhost')]
+#CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'https://'+ os.getenv('APP_DOMAIN','localhost')]
 # Application definition
 
 INSTALLED_APPS = [
