ac6a637d35b1c1188cc1b2ea40fade2160120a24
Mahaut Windal <mwindal@hotmail.com>
2026-03-20 14:32:00 +0100

    Add CI/CD setup with Docker and environment configuration

--- job_board/settings.py
+++ job_board/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v++r+fn6w$j+yr3ye5+_qnqepn*h(n@i$-@hem%b=t3@4za%wp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('APP_DEBUG', 'False') == True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', os.getenv('APP_DOMAIN','localhost')]
 
 
 # Application definition
